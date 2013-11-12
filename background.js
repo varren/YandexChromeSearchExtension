686ecbd4fff7a6e0f2bff3912f7d05f72e66dd5d
Varren <varren@gmail.com>
2013-11-12 07:41:43 +0400

    Initial commit

---
+++ background.js
@@ -0,0 +1,23 @@
+﻿var DEFAULT_IMG_SEARCH_URL = "http://images.yandex.ru/yandsearch?rpt=imagecbir&img_url=";
+var DEFAULT_TEXT_SEARCH_URL = "http://yandex.ru/yandsearch?text=";
+
+function openTabAfterCurrent(url){
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+	chrome.tabs.create({'url':url, 'index':tabs[0].index + 1,  'selected': false});
+  });
+}
+
+function searchYandex(info){
+  var searchstring = info.selectionText;
+  var url = DEFAULT_TEXT_SEARCH_URL + encodeURIComponent(searchstring);
+  openTabAfterCurrent(url);
+}
+
+function searchYandexImg(info){
+  var searchstring = info.srcUrl;
+  var url = DEFAULT_IMG_SEARCH_URL + searchstring;
+  openTabAfterCurrent(url);
+}
+
+chrome.contextMenus.create({title:"Искать в Яндексе", contexts:["selection"], onclick:searchYandex});
+chrome.contextMenus.create({title:"Искать в Яндексе", contexts:["image"], onclick:searchYandexImg});
